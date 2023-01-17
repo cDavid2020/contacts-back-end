66038c646b0364fd8823960b227f3f1df79636d9
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-01-17 10:05:47 -0600

    Add node "start" script to package.json

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+
+const app = express();
+// res = response. telling express server. if u get a request to the homepage, this is the response i want u to send back
+app.get("/", (_, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+// the underscore is a placeholder. we dont need to use it. we just need to tell express that we are expecting a request and a response
+app.listen(3000, () => {
+  console.info("Server is running on port 3000");
+});
