a2ffce5d59898f3cde8ba0fe2f38d1332310b9ae
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-01-17 10:25:54 -0600

    Add app.get & app.post

--- app/index.js
+++ app/index.js
@@ -3,8 +3,13 @@ import express from "express";
 const app = express();
 // res = response. telling express server. if u get a request to the homepage, this is the response i want u to send back
 app.get("/", (_, res) => {
-  res.send("<h1>Hello World</h1>");
+  response.send("<h1>Hello World</h1>");
 });
+
+app.post("/api/Contacts", (request, response) => {
+  cacheconsole.log(request.body);
+  response.send("ok");
+}
 // the underscore is a placeholder. we dont need to use it. we just need to tell express that we are expecting a request and a response
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
