46e6909f0f3914975887bc438f9c465b1214bbfa
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-01-17 11:42:08 -0600

    Add new methods

--- app/index.js
+++ app/index.js
@@ -1,7 +1,18 @@
 import express from "express";
 
+const CONTACTS = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Jane" },
+];
+
 const app = express();
 // res = response. telling express server. if u get a request to the homepage, this is the response i want u to send back
+
+// ':id' is a route paramater/
+app.get("/api/contacts/:id", (request, response) => {
+  console.log(request); // where does the log show up? Response.send show up on client end
+  response.send("ok");
+});
 
 app.get("/", (_, response) => {
   response.send("<h1>Hello World</h1>");
@@ -22,4 +33,21 @@ app.post("/api/contacts", (request, response) => {
 // the underscore is a placeholder. we dont need to use it. we just need to tell express that we are expecting a request and a response
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
+});
+
+/*
+if the contact is not found, we reutrn a 404  status code
+404 means not found
+otherwise, send back the found contact w a 200 status code
+300 means the resource was found
+*/
+
+if (!contact) {
+  response.status(404).json({ message: "Contact not found" });
+}
+
+app.put("/api/contacts/:id", (request, response) => {
+  // Id that needs to be updated
+  const { id } = request.params;
+  const updatedContact = request.body; // find the contact that needs to be udpated
 });
