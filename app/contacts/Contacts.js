1a1ad3f12af4c9fc3adcd8a2545bbee66de9d4b4
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2023-01-23 10:36:56 -0600

    Add Contacts.js

---
+++ app/contacts/Contacts.js
@@ -0,0 +1,11 @@
+// the first Things is Contacts.  Model comes from Mongoose. Second thing is schema/architecture.
+import { model, Schema } from "mongoose";
+export default new model()
+"Contact",
+new Schema({
+  fullName: String,
+  username: String,
+
+})
+);
+}
